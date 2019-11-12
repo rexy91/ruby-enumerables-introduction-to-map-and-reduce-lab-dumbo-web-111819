35acc8cc804d06f9d79a82f57466f0280736c14a
Yu Hao Ye <rxye1991@gmail.com>
2019-11-12 01:14:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,46 @@
 # My Code here....
+
+def map_to_negativize(array)
+  array.map{ |num|
+    num = num * -1 
+  }
+end 
+
+def map_to_no_change(array)
+  array.map{ |names|
+  names = names 
+  }
+end
+
+def map_to_double(array)
+  array.map do |num|
+    num = num * 2 
+  end
+end
+
+def map_to_square(array)
+  array.map do |num|
+    num *= num 
+  end
+end
+  
+
+def reduce_to_total(array, starting_point)
+    array.reduce(starting_point) { |sum, num| sum + num}
+end
+
+def reduce_to_total(array, starting_point=0)
+    array.reduce(starting_point) { |sum, num| sum + num}
+end
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
